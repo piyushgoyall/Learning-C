f0fc6e2f9d94290a9240625df2f18a4061958155
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-07-22 15:56:10 +0530

    Update and rename 13.cpp to basic/simple_interest.cpp

---
+++ basic/simple_interest.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int p,r,t,si;
+	printf("enter the value of p");
+	scanf("%d",&p);
+	printf("enter the value of r");
+	scanf("%d",&r);
+	printf("enter the value of t");
+	scanf("%d",&t);
+	d=(p*r*t)/100;
+	printf("%d",si);
+	
+}
