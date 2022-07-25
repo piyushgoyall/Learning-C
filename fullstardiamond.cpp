c761cd51c3a7ed5cafcf429c8f6793f5a9998486
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-07-25 17:50:35 +0530

    Add files via upload

---
+++ fullstardiamond.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	for(i=0;i<=4;i++)
+	{
+		for(j=0;j<=9;j++)
+		{
+			if(j<=4)
+			{
+				if(j<=4-i)
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+			else
+			{
+				if(j>=10-(5-i))
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+		}
+			printf("\n");
+	}
+	
+	for(i=0;i<=4;i++)
+	{
+		for(j=0;j<=9;j++)
+		{
+			if(j<=4)
+			{
+				if(j<=i)
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+			else
+			{
+				if(j>=9-i)
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
+		}
+			printf("\n");
+	}
+}
+
---
+++ star.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+    for(i=0;i<=4;i++)
+    {
+    	for(j=0;j<=i;j++)
+    	{
+    		printf("*\t");
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(i=0;i<=4;i++)
+	{
+		for(j=0;j<=4-i;j++)
+		{
+			printf("*\t");
+		}
+		printf("\n");
+	}
+	for(i=0;i<=4;i++)
+	{
+		for(j=0;j<=4;j++)
+		{
+			if(j>=4-i)
+			{
+				printf("*\t");
+			}
+			else
+			printf(" \t");
+		}
+		printf("\n");
+	    }
+	    
+	    printf("\n");
+	    
+	    for(i=0;i<=4;i++)
+	    {
+	    	for(j=0;j<=4;j++)
+	    	{
+	    		if(j>=i)
+	    		{
+	    			printf("*\t");
+				}
+				else
+				{
+					printf(" \t");
+				}
+			}
+		printf("\n");
+		}
+	
+	
+}
