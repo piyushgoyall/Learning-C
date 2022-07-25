da927cf1b1c3c2df0d6a8383533de892b5183eec
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-07-25 17:44:17 +0530

    Add files via upload

---
+++ strong-num.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int original_num,num,factorial=1,sum=0,i,j;
+	printf("enter the number ");
+	scanf("%d",&original_num);
+	for(num=original_num;num!=0;num=num/10 )
+	{
+		i=num%10;
+		printf("%d\t",i);
+	    factorial = 1;
+	    for(j=1;j<=i;j++)
+	    {
+	    	factorial = factorial*j;
+		}
+	    sum = sum+factorial;
+	}
+	if(sum==original_num)
+	{
+		printf("\n%d is a strong number",original_num);
+	}
+	else
+	{
+		printf("\n%d is not a strong number",original_num);
+	}
+}
