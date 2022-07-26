8190111d3216abc62afd0134d5e42968cfe8b5dc
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-07-26 11:04:42 +0530

    Add files via upload

---
+++ array-copy.cpp
@@ -0,0 +1,23 @@
+//wap to copy all elements of an array to another array
+#include<stdio.h>
+int main()
+{
+	int a[7],b[7],pos;
+
+	for(pos = 0;pos<=6;pos++)
+	{
+		printf("Enter the value for a[%d] ",pos);
+	    scanf("%d",&a[pos]);
+	}
+	for(pos = 0;pos<=6;pos++)
+	{
+		printf("Element of a[%d] = %d\n",pos,a[pos]);
+		b[pos] = a[pos];
+	}
+	printf("\n");
+   
+    for(pos=0; pos<=6;pos++)
+    {
+        printf("Element of b[%d] = %d\n",pos,b[pos]);
+    }
+}
---
+++ array-sum.cpp
@@ -0,0 +1,18 @@
+//wap to find sum of all arrat elements
+#include<stdio.h>
+int main()
+{
+	int a[5],pos,sum=0;
+	for(pos=0;pos<=4;pos++)
+	{
+		printf("enter the value for a[%d] ",pos);
+		scanf("%d",&a[pos]);
+	}
+	for(pos=0;pos<=4;pos++)
+	{
+		printf("Element of a[%d] = %d\n",pos,a[pos]);
+		sum=sum + a[pos];
+	}
+	printf("\n");
+	printf("Sum of array = %d",sum);
+}
