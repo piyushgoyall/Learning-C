b76590b9962ee210df1c1cffaf72b42499bdc618
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-07-26 12:19:17 +0530

    Update and rename array-reverse.cpp to array/array-reverse.cpp

--- array/array-reverse.cpp
+++ array/array-reverse.cpp
@@ -6,19 +6,19 @@ int main()
 	for(pos = 0;pos<=4;pos++)
 	{
 		printf("Enter the value for a[%d] ",pos);
-	    scanf("%d",&a[pos]);
+	        scanf("%d",&a[pos]);
 	}
 	for(pos = 0;pos<=4;pos++)
 	{
 		printf("Element of a[%d] = %d\n",pos,a[pos]);
 	}
 	printf("\n");
-    for(pos=0; pos<=4;pos++)
-    {
-        b[4-pos] = a[pos];
+        for(pos=0; pos<=4;pos++)
+        {
+                b[4-pos] = a[pos];
 		
-    }
-    for(pos = 0;pos<=4;pos++)
+        }
+       for(pos = 0;pos<=4;pos++)
 	{
 		printf("Element of b[%d] = %d\n",pos,b[pos]);
 	}
