da5bcc41d9eebd43fcec3592c72f2768feccbac6
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-07-29 16:18:23 +0530

    Add files via upload

---
+++ array-2/identity-matrix.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],row,column,flag=1;
+	for(row=0; row<=2; row++)
+	{
+		for(column=0; column<=2; column++)
+		{
+			printf("Enter any value ");
+			scanf("%d",&a[row][column]);
+		}
+	}
+	printf("\n");
+	for(row=0; row<=2; row++)
+	{
+		for(column=0; column<=2; column++)
+		{
+			printf("%d\t",a[row][column]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	for(row=0; row<=2; row++)
+	{
+		for(column=0; column<=2; column++)
+		{
+			if(row == column )
+			{
+				if(a[row][column]!=1)
+				{
+					flag=0;
+				}
+			}
+		}
+	}
+	if(flag==1)
+	printf("Identity matrix");
+	else
+	{
+		printf("Not an identity matrix");
+	}
+}
---
+++ array-2/upper-triangular-matrix.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	int a[3][3],row ,column;
+	for(row=0; row<=2; row++)
+	{
+		for(column=0; column<=2; column++)
+		{
+			printf("enter any value ");
+			scanf("%d",&a[row][column]);
+		}
+	}
+	printf("\n");
+	for(row=0; row<=2; row++)
+	{
+		for(column=0; column<=2; column++)
+		{
+			printf("%d\t",a[row][column]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	printf("Upper triangular matrix:\n");
+	for(row=0; row<=2; row++)
+	{
+		for(column=0; column<=2; column++)
+		{
+			if(row<=column)
+			{
+				printf("%d\t",a[row][column]);
+			}
+			else
+			{
+			printf("\t");
+		    }
+		}
+		printf("\n");
+	}
+}
+	
