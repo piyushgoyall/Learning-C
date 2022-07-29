7d11cd25edc5330801c5078a307538fd9a2a9201
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-07-29 09:59:40 +0530

    Add files via upload

---
+++ loop/for-if/prime-num.cpp
@@ -0,0 +1,26 @@
+//wap to enter any number and check if it is a prime number
+#include<stdio.h>
+int main()
+{
+	int number,i,flag=1;
+	printf("enter the number ");
+	scanf("%d",&number);
+	for(int i=2; i<number;i++) 
+    {
+	  if(number%i == 1)
+	  {    	
+	   flag = 1;
+	   break;
+	  }
+	  
+    }
+    
+    if(flag==0)
+    {
+    	printf("This is Prime Number");
+	}
+	else
+	{
+		printf("This is Not Prime Number");
+	}
+}
