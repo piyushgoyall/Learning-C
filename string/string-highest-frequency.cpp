34215529fb0841faf6c34f63f407e54ac59e93dd
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-01 11:22:21 +0530

    Add files via upload

---
+++ string/string-highest-frequency.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[10];
+	printf("Enter the string :- ");
+	gets(s);
+	printf("\n");
+	int l,count,j,max,i;
+	l = strlen(s);
+	max = 0;
+	for(i=0; i<l; i++)
+	{
+		count=0;
+		for(j=0; j<l; j++)
+		{
+			if(s[j] == s[i] && i!=j )
+			{
+				count = count+1;
+			}
+		}
+		if(count>max)
+		{
+			max = count;
+		}
+    }
+    for(i=0; i<l; i++)
+	{
+		count=0;
+		for(j=0; j<l; j++)
+		{
+			if(s[j] == s[i] && i!=j )
+			{
+				count = count+1;
+			}
+		}
+		if(count==max)
+		{
+			printf("Highest frequency = %c\n",s[i]);
+		}
+    }
+}
---
+++ string/string-lowest-frequency.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[10];
+	printf("Enter the string :- ");
+	gets(s);
+	printf("\n");
+	int l,j,i,count,min;
+	l = strlen(s);
+	min=l;
+	for(i=0; i<l; i++)
+	{
+		count=0;
+		for(j=0; j<l; j++)
+		{
+			if(s[j] == s[i] && i!=j)
+			{
+				count = count+1;
+			}
+		}
+		if(count<min)
+		{
+			min = count;
+		}
+	}
+	for(i=0; i<l; i++)
+	{
+		count=0;
+		for(j=0; j<l; j++)
+		{
+			if(s[j] == s[i] && i!=j )
+			{
+				count = count+1;
+			}
+		}
+		if(count==min)
+		{
+			printf("Lowest frequency = %c\n",s[i]);
+		}
+    }
+}
