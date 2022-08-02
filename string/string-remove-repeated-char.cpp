231fec308b1840a87b7cb7b500e71b1694877b82
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-02 15:51:08 +0530

    Add files via upload

---
+++ string/string-remove-repeated-char.cpp
@@ -0,0 +1,27 @@
+//wap to remove all repeated characters from a given string
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int repeat[0],r=0,i,j,l;
+	int flag=1;
+	char s[10];
+	printf("Enter the string :- ");
+	gets(s);
+	l = strlen(s);
+	for(i=0; i<l; i++)
+	{
+		flag=1;
+		for(j=0;j<l;j++)
+		{
+			if(s[i]==s[j] && i!=j)
+			{
+				flag=0;
+			}
+		}
+		if(flag==1)
+		{
+			printf("%c",s[i]);
+		}		
+	}
+}
