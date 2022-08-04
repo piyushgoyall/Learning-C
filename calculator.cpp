84440303b63b9276230283a8dfc24ec38e13bc6a
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-04 09:44:19 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	int a,b,c,d,e,f;
-    char t;
+    	char t;
 	printf("enter the input like 12+15 then press enter");
 	scanf("%d%c%d",&a,&t,&b);
 	c=a+b;
@@ -11,21 +11,17 @@ int main()
 	f=a/b;
 	switch(t)
 	{
-    case '+':
-	printf("%d",c);
-	break;
-	case '-':
-	printf("%d",d);
-	break;
-	case '*':
-	printf("%d",e);
-	break;
-	case '/':
-	printf("%d",f);
-	break;
-	
-	
+    		case '+':
+		printf("%d",c);
+		break;
+		case '-':
+		printf("%d",d);
+		break;
+		case '*':
+		printf("%d",e);
+		break;
+		case '/':
+		printf("%d",f);
+		break;
 	}
-	
-	
 }
