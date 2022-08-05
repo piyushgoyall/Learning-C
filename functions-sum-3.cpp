0efb701d72cdd709b9b1da55de1689366fcc5b1a
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-05 09:43:13 +0530

    Add files via upload

---
+++ functions-sum-3.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void sum(int a, int b)
+{
+	int c=a+b;
+	printf("%d",c);
+}
+int main()
+{
+	sum(10,20);
+}
---
+++ functions-sum-4.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int sum(int a, int b)
+{
+	int c=a+b;
+	return c;
+}
+int main()
+{
+	int d= sum(16,4);
+	printf("%d",d);
+}
