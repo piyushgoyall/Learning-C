5eb9e67a8009bd0a05eb132ba4a4d4f733bffc36
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-07 18:24:10 +0530

    Update days_in_month.cpp

--- switch/days_in_month.cpp
+++ switch/days_in_month.cpp
@@ -7,42 +7,42 @@ int main()
 	switch(a)
 	{
 		case'january':
-		printf("30");
-		break;
+			printf("30");
+			break;
 		case'februuary':
-		printf("28");
-		break;
+			printf("28");
+			break;
 		case'march':
-		printf("31");
-		break;
+			printf("31");
+			break;
 		case'april':
-		printf("30");
-		break;
+			printf("30");
+			break;
 		case'may':
-		printf("31");
-		break;
+			printf("31");
+			break;
 		case'june':
-		printf("30");
-		break;
+			printf("30");
+			break;
 		case'july':
-		printf("31");
-		break;
+			printf("31");
+			break;
 		case'august':
-		printf("31");
-		break;
+			printf("31");
+			break;
 		case'september':
-		printf("30");
-		break;
+			printf("30");
+			break;
 		case'october':
-		printf("31");
-		break;
+			printf("31");
+			break;
 		case'november':
-		printf("30");
-		break;
+			printf("30");
+			break;
 		case'december':
-		printf("31");
-		break;
+			printf("31");
+			break;
 		default:
-		printf("invalid");
+			printf("invalid");
 	}
 }
