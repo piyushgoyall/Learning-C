0873cc3adf83c1e34bb2cb37d6c850c487bb0e5c
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-07 18:26:07 +0530

    Update vowel3.0.cpp

--- switch/vowel3.0.cpp
+++ switch/vowel3.0.cpp
@@ -1,25 +1,24 @@
 #include<stdio.h>
 int main()
 {
-  char c;
-		printf("enter the value of c");
-		scanf("%c",&c);
-		switch(c)
-		{
-			case'A':
-			case'E':
-		    case'I':
-		    case'O':
-		    case'U':	
-			case'a':
-			case'e':
-			case'i':
-			case'o':
-			case'u':
+	char c;
+	printf("enter the value of c");
+	scanf("%c",&c);
+	switch(c)
+	{
+		case'A':
+		case'E':
+	    	case'I':
+	    	case'O':
+	    	case'U':	
+		case'a':
+		case'e':
+		case'i':
+		case'o':
+		case'u':
 			printf("vowel");
 			break;
-			default:
+		default:
 			printf("consonant");	
-		}
-		
+	}		
 }
