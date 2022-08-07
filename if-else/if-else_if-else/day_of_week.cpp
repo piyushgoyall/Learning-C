07cd800fb79c922f076afc217e0dc6b3b337bf81
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-07 18:10:46 +0530

    Update day_of_week.cpp

--- if-else/if-else_if-else/day_of_week.cpp
+++ if-else/if-else_if-else/day_of_week.cpp
@@ -6,31 +6,31 @@ int main()
 	scanf("%d",&a);
 	if(a ==1)
 	{
-		printf("the day is monday");
+		printf("today is monday");
 	}
 	else if(a == 2)
 	{ 
-	printf("today is tuesday");
+		printf("today is tuesday");
 	}
 	else if(a == 3)
 	{ 
-	printf("today is wednesday");
+		printf("today is wednesday");
 	}
 	else if(a == 4)
 	{ 
-	printf("today is thursday");
+		printf("today is thursday");
 	}
 	else if(a == 5)
 	{ 
-	printf("today is friday");
+		printf("today is friday");
 	}
 	else if(a == 6)
 	{ 
-	printf("today is saturday");
+		printf("today is saturday");
 	}
 	else if(a == 7)
 	{ 
-	 printf("today is sunday");
+	 	printf("today is sunday");
 	}
 	else 
 	{
