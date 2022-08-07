767199b654d3783601c1b9428a5f7c7ef67de2b9
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-07 18:28:11 +0530

    Update factorial.cpp

--- loop/factorial.cpp
+++ loop/factorial.cpp
@@ -3,8 +3,8 @@
 int main()
 {
 	int number,i,factorial=1;
-    printf("enter the number ");
-    scanf("%d",&number);
+    	printf("enter the number ");
+    	scanf("%d",&number);
 	for(int i=1; i<=number; i++)
 	{
 		factorial=factorial*i;
