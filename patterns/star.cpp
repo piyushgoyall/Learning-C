fa08645bf9b985b8e1325323de3a2c0e821de819
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-08 17:18:57 +0530

    Update star.cpp

--- patterns/star.cpp
+++ patterns/star.cpp
@@ -1,53 +1,51 @@
 #include<stdio.h>
 int main()
 {
-    int i,j;
-    for(i=0;i<=4;i++)
-    {
-    	for(j=0;j<=i;j++)
+        int i,j;
+    	for(i=0;i<=4;i++)
     	{
-    	       	printf("*\t");
-	}
-	printf("\n");
-    }
-    printf("\n");
-    for(i=0;i<=4;i++)
-    {
-         for(j=0;j<=4-i;j++)
-	 {
-		printf("*\t");
-	 }
-		printf("\n");
-     }
-     for(i=0;i<=4;i++)
-     {
-	 for(j=0;j<=4;j++)
-	 {
-		if(j>=4-i)
-		{
-			printf("*\t");
+    		for(j=0;j<=i;j++)
+    		{
+    	       		printf("*\t");
 		}
-		else
-		printf(" \t");
-	  }
-          printf("\n");
-	  }
-	    
-          printf("\n");
-	    
+		printf("\n");
+    	}
+    	printf("\n");
+    	for(i=0;i<=4;i++)
+    	{
+        	 for(j=0;j<=4-i;j++)
+	 	 {
+			printf("*\t");
+	 	 }
+		 printf("\n");
+     	}
+     	for(i=0;i<=4;i++)
+     	{
+	 	for(j=0;j<=4;j++)
+	 	{
+			if(j>=4-i)
+			{
+				printf("*\t");
+			}
+			else
+			printf(" \t");
+	  	}
+          	printf("\n");
+	  }  
+          printf("\n"); 
 	  for(i=0;i<=4;i++)
 	  {
-	      for(j=0;j<=4;j++)
-	      {
-	    	   if(j>=i)
-	    	   {
-	        	 printf("*\t");
-		   }
-		   else
-		   {
-			 printf(" \t");
-		   }
-	      }
-	printf("\n");
+	      	for(j=0;j<=4;j++)
+	      	{
+	    	   	if(j>=i)
+	    	   	{
+	        	 	printf("*\t");
+		   	}
+		   	else
+		   	{
+			 	printf(" \t");
+		   	}
+	      	}
+		printf("\n");
 	  }
 }
