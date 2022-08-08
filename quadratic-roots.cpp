2fe33bfdb070e7ca749d674c6155f733ec128bf0
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-08 12:33:02 +0530

    Add files via upload

---
+++ quadratic-roots.cpp
@@ -0,0 +1,52 @@
+//C program to find roots of a quadratic equation (integer cofficients only)
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c;
+	float d,e,f,g,h,root_1,root_2,real,imag;
+	printf("Enter the cofficient of x^2 : ");
+	scanf("%d",&a);
+	printf("\n");
+	printf("Enter the cofficient of x : ");
+	scanf("%d",&b);
+	printf("\n");
+	printf("Enter the constant term : ");
+	scanf("%d",&c);
+	printf("\n");
+	printf("Entered quadratic equation : %dx^2 + (%d)x + %d",a,b,c);
+	printf("\n");
+	printf("\n");
+	f = pow(b,2);
+	g = 4*a*c;
+	d = f - g;
+	e = sqrt(d);
+	h = 2*a;
+	if(d>0)
+	{
+		root_1 = (-b+e)/h;
+		root_2 = (-b-e)/h;
+		printf("Root-1 : %f",root_1);
+		printf("\n");
+		printf("\n");
+		printf("Root-2 : %f",root_2);
+	}
+	else if(d==0)
+	{
+		root_1=root_2 = -b/h;
+		printf("Equal roots with value : %f",root_1);
+	}
+	else
+	{
+		real = -b/h;
+		imag=sqrt(-d)/h;
+		if(real==0)
+		{
+			printf(" Imaginary roots with value : %.0f i and -%.0f i",imag,imag);
+		}
+		else
+		{
+			printf("Imaginary roots with value : %.0f + %.0f i and %.0f - %.0f i",real,imag,real,imag);
+		}
+	}
+}
