0e937ecd0489ea35f96a014d1ebf3db66834ecba
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2022-08-08 16:42:13 +0530

    Add files via upload

---
+++ recursion/reverse-recursion.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+int reverse(int n)
+{
+	int static rev =0;
+	
+	if(n>0)
+	{
+		rev = rev*10+(n%10);
+		
+		reverse(n/10);
+	}
+	else
+	{
+		return rev;
+	}
+}
+
+int main()
+{
+	int n;
+	printf("Enter the number : ");
+	scanf("%d",&n);
+	printf("\n");
+	int d = reverse(n);
+	printf("Reverse of number : %d",d);
+}
