b2e97b33e00a9faf055b5fbdc3c9b5003f859595
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-15 15:12:05 +0530

    Add files via upload

---
+++ loop/prime-series.cpp
@@ -0,0 +1,26 @@
+//wap to print all prime numbers between 2 to n
+#include<stdio.h>
+int main()
+{
+	int a,b,i,flag;
+	printf("Enter the starting value : ");
+	scanf("%d",&a);
+	printf("Enter the ending value : ");
+	scanf("%d",&b);
+	while(a<=b&&a>=2)
+	{
+		flag=1;
+		for(i=2;i<a;i++)
+		{
+			if(a%i==0)
+			{
+				flag=0;
+			}
+		}
+		if(flag==1)
+		{
+			printf("%d\t",a);
+		}
+		a=a+1;
+	}
+}
