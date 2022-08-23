d1b00df247c08a9fe07f360691046b411c9b6cf9
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-23 09:35:21 +0530

    Add files via upload

---
+++ recursion/recursion-array.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int array(int a[5])
+{
+	int static i=0;
+	if(i<=4)
+	{
+		printf("%d\t",a[i]);
+		i=i+1;
+		array(a);
+	}
+}
+int main()
+{
+	int a[5],i;
+	for(i=0;i<=4;i++)
+	{
+		printf("Enter the value for a[%d] : ",i);
+		scanf("%d",&a[i]);
+		printf("\n");
+		
+	}
+	printf("Elements of the array :  ");
+	array(a);
+}
