edf5a7cb80cb605f53a8b180fc01d07016e57c79
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-25 21:49:58 +0530

    Update pb-BMI.cpp

--- practice/pb-BMI.cpp
+++ practice/pb-BMI.cpp
@@ -1,3 +1,5 @@
+/*The Body Mass Index (BMI) is defined as ratio of the weight of a person (in kilograms) to the square of the height (in meters). 
+Write a program that receives weight and height, calculates the BMI, and reports the BMI category.*/
 #include<stdio.h>
 #include<math.h>
 int main()
