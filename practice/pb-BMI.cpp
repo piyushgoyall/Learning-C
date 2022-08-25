3bd322971b07cfb31e138f8372967980e414bda1
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-25 21:46:46 +0530

    Add files via upload

---
+++ practice/pb-BMI.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float height,weight,BMI;
+	printf("Enter the Weight(in Kg) : ");
+	scanf("%f",&weight);
+	printf("\n");
+	printf("Enter the Height(in meters) : ");
+	scanf("%f",&height);
+	printf("\n");
+	BMI=weight/pow(height,2);
+	printf("BMI : %f\n\n",BMI);
+	printf("Category : "); 
+	if(BMI<15)
+	{
+		printf("Starvation");
+	}
+	else if(BMI>=15.1 && BMI<=17.5)
+	{
+		printf("Anorexic");
+	}
+	else if(BMI>17.5 && BMI<=18.5)
+	{
+		printf("Underweight");
+	}
+	else if(BMI>18.5 && BMI<=24.9)
+	{
+		printf("Ideal");
+	}
+	else if(BMI>24.9 && BMI<=29.9)
+	{
+		printf("Overweight");
+	}
+	else if(BMI>29.9 && BMI<=30.9)
+	{
+		printf("Obese");
+	}
+	else if(BMI>30.9)
+	{
+		printf("Morbidly Obese");
+	}
+}
