2d0d444a472a9570a6e3db8b3b44490b8bb58ae5
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-08-27 14:44:47 +0530

    Add files via upload

---
+++ practice/pb-octal.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int num,rem,oct[8],i=0,k;
+	printf("Enter the number : ");
+	scanf("%d",&num);
+	printf("\n");
+	while(num>0)
+	{
+		rem=num%8;
+		num=num/8;
+		oct[i]=rem;
+		i++;
+	}
+	printf("Octal for the entered decimal number : ");
+	for(k=i-1;k>=0;k--)
+	{
+		printf("%d",oct[k]);
+	}
+}
