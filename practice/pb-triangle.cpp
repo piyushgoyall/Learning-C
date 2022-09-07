5950990d250cee2833df002c0681f69ea402a5cc
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-09-07 17:44:44 +0530

    Update pb-triangle.cpp

--- practice/pb-triangle.cpp
+++ practice/pb-triangle.cpp
@@ -1,3 +1,5 @@
+/*Write a program to check whether a triangle is valid or not, when the three angles of the triangle are entered through the
+keyboard. A triangle is valid if the sum of all the three angles is equal to 180 degrees.*/
 #include<stdio.h>
 int main()
 {
