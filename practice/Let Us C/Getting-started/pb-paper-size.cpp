b6e79e58acf8122d359e9ef264203d7e09aab225
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-09-07 16:34:13 +0530

    Rename practice/pb-paper-size.cpp to practice/Let Us C/Getting-started/pb-paper-size.cpp

--- practice/Let Us C/Getting-started/pb-paper-size.cpp
+++ practice/Let Us C/Getting-started/pb-paper-size.cpp
@@ -1,5 +1,5 @@
 /*Paper of size A0 has dimensions 1189 mm x 841 mm. Each subsequent size A(n) is defined as A(n-1) cut in half parallel to its shorter sides.
-Write a program to calculate and print paper sizes A0, A1, A2, ….. A8.*/
+Write a program to calculate and print paper sizes A0, A1, A2, â€¦.. A8.*/
 #include<stdio.h>
 int main()
 {
