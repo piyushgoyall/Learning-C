0f5f62b3e6bee64ef7281d8dd6873bb8a086d07b
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2022-09-07 16:36:20 +0530

    Update and rename practice/pb-digit-sum.cpp to practice/Let Us C/C Instructions/pb-digit-sum.cpp

--- practice/Let Us C/C Instructions/pb-digit-sum.cpp
+++ practice/Let Us C/C Instructions/pb-digit-sum.cpp
@@ -1,3 +1,5 @@
+/*If a five-digit number is input through the keyboard, write a program to calculate the sum of its digits.
+(Hint: Use the modulus operator ‘%’) */
 #include<stdio.h>
 int main()
 {
