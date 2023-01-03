47a2aea3c2c00ea8472c929a4830ad5966afea1a
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2023-01-03 21:48:30 +0530

    Add files via upload

---
+++ switch/calculator-switch-case.c
@@ -0,0 +1,38 @@
+
+#include<stdio.h>
+int main()
+{
+	char op;
+	printf("Enter the operation you want to perform : ");
+	scanf("%c",&op);
+	printf("\n");
+
+	float num1,num2;
+	printf("Enter first number : ");
+	scanf("%f",&num1);
+	printf("\n");
+	printf("Enter second number : ");
+	scanf("%f",&num2);
+	printf("\n");
+
+    printf("Answer : ");
+	float c=num1+num2;
+	float d=num1-num2;
+	float e=num1*num2;
+	float f=num1/num2;
+	switch(op)
+	{
+    	case '+':
+			printf("%.3f",c);
+			break;
+		case '-':
+			printf("%.3f",d);
+			break;
+		case '*':
+			printf("%.3f",e);
+			break;
+		case '/':
+			printf("%.3f",f);
+			break;
+	}
+}
