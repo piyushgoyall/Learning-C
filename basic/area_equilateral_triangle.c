331bb65c9df08c13dff7720cebf0cba5fca998d5
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-01-11 22:38:28 +0530

    update

---
+++ basic/area_equilateral_triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of a : ");
+	scanf("%d",&a);
+	b=3;
+	c=(sqrt(b)*a*a)/4;
+	printf("%d",c);
+    return 0;
+}
