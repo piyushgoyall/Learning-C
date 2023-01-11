e8cb54a780d1d262a1e755a46f83128b5c95a3cf
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2023-01-11 13:25:56 +0530

    Update Matchstick-game.cpp

--- Matchstick-game.cpp
+++ Matchstick-game.cpp
@@ -4,6 +4,7 @@ computer always wins. Rules for the game are as follows:
 - The computer asks the player to pick 1, 2, 3, or 4 matchsticks.
 - After the person picks, the computer does its picking.
 - Whoever is forced to pick up the last matchstick loses the game.*/
+
 #include <stdio.h>
 int main()
 {
