e96c0bb318903d18947a378e05eb8df2ae7bb0e6
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-01-11 13:24:32 +0530

    test2
    too many tests

--- Matchstick-game.cpp
+++ Matchstick-game.cpp
@@ -4,25 +4,25 @@ computer always wins. Rules for the game are as follows:
 - The computer asks the player to pick 1, 2, 3, or 4 matchsticks.
 - After the person picks, the computer does its picking.
 - Whoever is forced to pick up the last matchstick loses the game.*/
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int pl,m=21,comp;
-	while(m>1)
+	int pl, m = 21, comp;
+	while (m > 1)
 	{
-		printf("Total matchsticks = %d\n\n",m);
+		printf("Total matchsticks = %d\n\n", m);
 		printf("Pick 1, 2, 3 or 4 matchsticks : ");
-		scanf("%d",&pl);
-		if(pl<=4&&pl>=1)
+		scanf("%d", &pl);
+		if (pl <= 4 && pl >= 1)
 		{
-			m=m-pl;
-			printf("Number of matchsticks left : %d\n\n",m);
-			comp=5-pl;
-			printf("Out of which computer picked : %d\n\n",comp);
-			m=m-comp;
-			if(m==1)
+			m = m - pl;
+			printf("Number of matchsticks left : %d\n\n", m);
+			comp = 5 - pl;
+			printf("Out of which computer picked : %d\n\n", comp);
+			m = m - comp;
+			if (m == 1)
 			{
-				printf("Number of matchsticks left : %d\n\n",m);
+				printf("Number of matchsticks left : %d\n\n", m);
 				printf("You loose the game");
 				break;
 			}
@@ -33,5 +33,4 @@ int main()
 			break;
 		}
 	}
-	
 }
