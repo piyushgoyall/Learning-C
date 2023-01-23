e811c30d07dba6b5e25fed93fba4b491d2b7e9e7
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-01-23 11:28:32 +0530

    update

--- practice/Uni. lab/23-01-2023(3).c
+++ practice/Uni. lab/23-01-2023(3).c
@@ -9,16 +9,16 @@ int main()
     switch (a)
     {
         case 1:
-            printf("The value is 1");
+            printf("The value is 1\n");
             break;
         case 2:
-            printf("The value is 2");
+            printf("The value is 2\n");
             break;
         case 3:
-            printf("The value is 3");
+            printf("The value is 3\n");
             break;
         default:
-            printf("Try again");
+            printf("Try again\n");
             break;
     }
     return 0;
