7b7626609c00122d6a5be125533a398fa4e1a0ad
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-02-22 14:39:39 +0530

    update

--- practice/Uni. lab/22-02-2023(1).c
+++ practice/Uni. lab/22-02-2023(1).c
@@ -1,27 +1,30 @@
 // Even or Odd using function.
 
-#include<stdio.h>
-int eoo(int *num)
-{
-    *num%2==0?printf("Even"):printf("Odd");
-}
-int main()
-{
-    int num;
-    scanf("%d",&num);
-    eoo(&num);
-    return 0;
-}
-
 // #include<stdio.h>
-// int eoo(int num)
+// int eoo(int *num)
 // {
-//     num%2==0?printf("Even"):printf("Odd");
+//     *num%2==0?printf("Even"):printf("Odd");
 // }
 // int main()
 // {
 //     int num;
 //     scanf("%d",&num);
-//     eoo(num);
+//     eoo(&num);
 //     return 0;
 // }
+
+#include<stdio.h>
+
+int eoo(int num);
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    eoo(num);
+    return 0;
+}
+
+int eoo(int num)
+{
+    num%2==0?printf("Even"):printf("Odd");
+}
