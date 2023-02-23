91d4fc6ceb498629634a6256d33aad778a934ff0
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-02-23 11:41:05 +0530

    even odd series

---
+++ practice/Uni. lab/23-02-2023(2).c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void odd();
+void even();
+int n = 1;
+void odd()
+{
+    if (n <= 10)
+    {
+        printf("%d ", n + 1);
+        n++;
+        even();
+    }
+    return;
+}
+void even()
+{
+    if (n <= 10)
+    {
+        printf("%d ", n - 1);
+        n++;
+        odd();
+    }
+    return;
+}
+int main()
+{
+    odd();
+}
