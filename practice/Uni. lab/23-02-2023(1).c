16112bab31a7e7a24020568008782c73b4d33cfa
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-02-23 11:37:51 +0530

    fibonacci series

---
+++ practice/Uni. lab/23-02-2023(1).c
@@ -0,0 +1,39 @@
+// fibonacci series with and without recursion.
+
+#include <stdio.h>
+int fibo(int i)
+{
+    if (i == 0 || i == 1)
+        return i;
+    else
+        return fibo(i - 1) + fibo(i - 2);
+}
+int main()
+{
+    int i, n;
+    printf("Enter a digit for fibonacci series: ");
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+        printf(" %d ", fibo(i));
+    return 0;
+}
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int term;
+//     printf("Enter value : ");
+//     scanf("%d",&term);
+//     int a=0,b=1;
+//     int sum=0;
+//     printf("%d  %d  ",a,b);
+//     for(int i=0;i<term-2;i++)
+//     {
+//         sum = a+b;
+//         printf("%d  ",sum);
+//         a=b;
+//         b=sum;
+//     }
+//     return 0;
+// }
