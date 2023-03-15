09fb4fb4fa114545295659e489dbbc25607a99f4
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-03-15 11:18:39 +0530

    array

---
+++ practice/Uni. lab/15-03-2023(1).c
@@ -0,0 +1,10 @@
+// Input and Output Array Elements
+
+#include <stdio.h>
+int main()
+{
+    int arr[5] = {19, 10, 8, 17, 9};
+    printf("Enter your number: ");
+    scanf("%d", &arr[2]);
+    printf("%d", arr[2]);
+}
