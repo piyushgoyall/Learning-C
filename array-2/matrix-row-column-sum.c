d27d83903995b89e9088c65d735084132a00a719
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2023-04-05 01:24:33 +0530

    Update matrix-row-column-sum.c

--- array-2/matrix-row-column-sum.c
+++ array-2/matrix-row-column-sum.c
@@ -1,42 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int a[3][3],row,column,sum=0;
-	for(row=0; row<=2; row++)
-	{
-		for(column=0; column<=2; column++)
-		{
-			printf("Enter any value ");
-			scanf("%d",&a[row][column]);
-		}
-	}
-	printf("\n");
-	for(row=0; row<=2; row++)
-	{
-		for(column=0; column<=2; column++)
-		{
-			printf("%d\t",a[row][column]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-	for(row=0; row<=2; row++)
-	{
-		sum = 0;
-		for(column=0; column<=2; column++)
-		{
-			sum=sum+a[row][column];
-		}
-	printf("Sum of row = %d\t",sum);
-	}
-	printf("\n");
-	for(row=0; row<=2; row++)
-	{
-		sum = 0;
-		for(column=0; column<=2; column++)
-		{
-			sum=sum+a[column][row];
-		}
-	printf("Sum of column = %d\t",sum);
-	}
+    int a[3][3], row, column, sum = 0;
+    for (row = 0; row <= 2; row++)
+    {
+        for (column = 0; column <= 2; column++)
+        {
+            printf("Enter any value ");
+            scanf("%d", &a[row][column]);
+        }
+    }
+    printf("\n");
+    for (row = 0; row <= 2; row++)
+    {
+        for (column = 0; column <= 2; column++)
+        {
+            printf("%d\t", a[row][column]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (row = 0; row <= 2; row++)
+    {
+        sum = 0;
+        for (column = 0; column <= 2; column++)
+        {
+            sum = sum + a[row][column];
+        }
+        printf("Sum of row = %d\t", sum);
+    }
+    printf("\n");
+    for (row = 0; row <= 2; row++)
+    {
+        sum = 0;
+        for (column = 0; column <= 2; column++)
+        {
+            sum = sum + a[column][row];
+        }
+        printf("Sum of column = %d\t", sum);
+    }
 }
