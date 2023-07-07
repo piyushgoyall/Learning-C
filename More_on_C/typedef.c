8669d4714ff7bf4482dc08a230e796eaad229177
Piyush Goyal <109795757+piyushgoyall@users.noreply.github.com>
2023-07-07 22:56:34 +0530

    Create typedef.c

---
+++ More_on_C/typedef.c
@@ -0,0 +1,13 @@
+// typedef
+
+#include <stdio.h>
+int main()
+{
+    typedef unsigned long ul;
+    ul l1, l2, l3;
+    typedef int integer;
+    integer a = 4;
+
+    printf("Value of integer is: %d", a);
+    return 0;
+}
