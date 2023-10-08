23f3214172fec28677b8f76f46bbdfe37bd9f2de
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-10-08 23:34:23 +0530

    new

---
+++ More_on_C/6-10-2023_(1).cpp
@@ -0,0 +1,33 @@
+// Revising recursion
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// void fun(int n)
+// {
+//     if (n > 0)
+//     {
+//         cout << n << "  ";
+//         fun(n - 1);
+//     }
+// }
+// int main()
+// {
+//     int res = 3;
+//     fun(res);
+//     return 0;
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int factorial(int n)
+// {
+//     if (n == 0)
+//     {
+//         return 1;
+//     }
+//     else
+//     {
+//         return n * factorial(n - 1);
+//     }
+// }
