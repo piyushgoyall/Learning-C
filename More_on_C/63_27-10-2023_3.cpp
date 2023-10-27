995ab4086a9fbcd9a85dfb21848903af400039ae
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2023-10-28 02:27:01 +0530

    update

---
+++ More_on_C/63_27-10-2023_3.cpp
@@ -0,0 +1,3 @@
+// Create three classes by name Day, Month and Year to store the day of the month, the month in two digits and the year in four digits respectively from the user.
+// Now the print the entire date as dd-mm-yyyy format in main function().
+
