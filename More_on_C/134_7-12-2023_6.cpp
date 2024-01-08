db77051ee7ba74c8e23883d9438b04a20a169d97
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-01-08 17:27:22 +0530

    new

--- More_on_C/134_7-12-2023_6.cpp
+++ More_on_C/134_7-12-2023_6.cpp
@@ -1,24 +1,56 @@
 // SET / MULTISET
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <set>
+
 using namespace std;
-int main()
-{
+
+int main() {
     // initializing set in ascending order
-    set<int> s1 = {23,45,12,34,7,45};
-    cout<<"s1 elements: ";
-    for(auto i:s1)
-    {
-        cout<<i<<" ";
-    }   
+    set<int> s1 = {23, 45, 12, 34, 7, 45};
+    cout << "s1 elements: ";
+    for (auto i : s1) {
+        cout << i << " ";
+    }
 
-    // initializing set in descending order
-    mutliset<int>
+    // initializing multiset in descending order
+    multiset<int, greater<>> s2 = {23, 45, 12, 34, 7, 45};
+    cout << "\ns2 elements: ";
+    for (auto i : s2) {
+        cout << i << " ";
+    }
 
-    // inserting element
+    // inserting element into set s1
     s1.insert(100);
-    cout<<"\ns1 elements (after insert): ";
-    for(auto)
+    cout << "\ns1 elements (after insert): ";
+    for (auto i : s1) {
+        cout << i << " ";
+    }
 
     return 0;
 }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     // initializing set in ascending order
+//     set<int> s1 = {23,45,12,34,7,45};
+//     cout<<"s1 elements: ";
+//     for(auto i:s1)
+//     {
+//         cout<<i<<" ";
+//     }   
+
+//     // initializing set in descending order
+//     mutliset<int>
+
+//     // inserting element
+//     s1.insert(100);
+//     cout<<"\ns1 elements (after insert): ";
+//     for(auto)
+
+//     return 0;
+// }
---
+++ More_on_C/135_8-1-2023_7.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char marks[100] = {'a','b','c','d'};
+    cout<<marks[5];
+
+    // int marks[100];
+
+    // int n;
+    // cout << "Enter number of students: ";
+    // cin >> n;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> marks[i];
+    // }
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << marks[i] << " ";
+    // }
+
+    // cout << "\n"
+    //      << marks[94];
+
+    return 0;
+}
