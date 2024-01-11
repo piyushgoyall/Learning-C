774d05c9af1fff175662173a7eae10c9e85bdbc1
piyushgoyall <109795757+piyushgoyall@users.noreply.github.com>
2024-01-11 23:19:05 +0530

    new

---
+++ More_on_C/141_11-1-2024_1.cpp
@@ -0,0 +1,76 @@
+// CUMULATIVE SUM APPROACH / PREFIX SUM APPROACH
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    
+    return 0;
+}
+
+// // MAX SUM OF SUBARRAY
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     int maxSum = INT_MIN;
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i; j < n; j++)
+//         {
+//             int sum = 0;
+//             for (int k = i; k <= j; k++)
+//             {
+//                 sum += arr[k];
+//                 // cout << arr[k] << " ";
+//             }
+//             maxSum = max(maxSum, sum);
+//         }
+//     }
+//     cout << maxSum << endl;
+//     return 0;
+// }
+
+// // PRINT SUBARRAY
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> arr[i];
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i; j < n; j++)
+//         {
+//             for (int k = i; k <= j; k++)
+//             {
+//                 cout << arr[k] << " ";
+//             }
+//             cout << endl;
+//         }
+//     }
+//     return 0;
+// }
